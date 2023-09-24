1943af0e92e32312c919950d3dbd2054397689cf
Cleyh <unbd_233@qq.com>
2023-09-24 13:20:43 +0800

    Initial commit

---
+++ src/main/java/org/mcug/fgj/FGJ.java
@@ -0,0 +1,17 @@
+package org.mcug.fgj;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FGJ extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
